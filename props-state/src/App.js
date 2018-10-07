59e933f6e3764c7f4de36e6ff52cc0711a5c968a
Raul Larriega <raulgf_02@hotmail.com>
2018-10-07 17:33:07 -0500

    Props +2

--- props-state/src/App.js
+++ props-state/src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Contador extends Component {
-  constructor (){
-    super()
-    this.state = { contador: 1}
+  constructor (props){
+    super(props)
+    console.log(this.props.contadorInicial)
+    this.state = { contador: this.props.contadorInicial}
     //El metodo setState es asincrono
     setInterval(() => this.setState({ contador: this.state.contador + 1}), 1000)
   }
   render(){
     //const contador = 0
-    return <span>{ this.state.contador }</span>
+    //return <span>{ this.state.contador }</span>
+    return <ContadorNumero numero={ this.state.contador }/>
+  }
+}
+//Inicializando props
+Contador.defaultProps = {
+  contadorInicial: 0
+}
+
+class ContadorNumero extends Component {
+  render() {
+    console.log('ContadorNumero render()')
+    return <span>{ this.props.numero }</span>
   }
 }
 class App extends Component {
@@ -18,7 +31,7 @@ class App extends Component {
     return (
       <div className="App">
         <p>Primer componente con state</p>
-        <Contador/>
+        <Contador contadorInicial={100}/>
       </div>
     );
   }
