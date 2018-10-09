10efcdaaede703a1f89a548aae69d205d433d5dd
Raul Larriega <raulgf_02@hotmail.com>
2018-10-08 19:41:06 -0500

    Rendering Conditional

--- condicional/src/App.js
+++ condicional/src/App.js
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import ConditionalSection from './sections/conditional.js'
+import person from './data/person.json'
 
+class PersonItem extends Component {
+  render(){
+    const {person} = this.props
+    return (
+      <li>
+        <p><strong>Nombre:</strong>{person.name}, <strong>Edad:</strong>{person.edad}</p>
+      </li>
+    )
+  }
+}
 class App extends Component {
   render() {
+    const numbers = [1,1,2,3,4]
     return(
       <div className="App">
-        <ConditionalSection/>
+        {<ConditionalSection/>}
+        {/*Usando Listas*/}
+        { numbers.map((n,index) => {
+          return <p key={index}>Soy el numero {n}</p>
+        })}
+        {/*Usando objetos*/}
+        <ul>
+          {
+            person.map(p => {
+              return (
+                <PersonItem key={p.id} person={p}/>
+              )
+            })
+          }
+        </ul>
       </div>
     )
   }
