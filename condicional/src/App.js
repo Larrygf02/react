0c9fd4345f10b6b63e4e61568e11684ba3af0982
Raul Larriega <raulgf_02@hotmail.com>
2018-10-13 23:56:41 -0500

    Forms and children

--- condicional/src/App.js
+++ condicional/src/App.js
@@ -1,10 +1,22 @@
+
+
 import React, { Component } from 'react';
-import Forms from './sections/form.js'
+class Box extends Component {
+    render() {
+        return (
+            <div style={{ border: '1px solid #09f', margin:5, padding:5 }}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
 class App extends Component {
   render() {
     return(
       <div className="App">
-        <Forms />
+        <h4>Children Props</h4>
+        <Box >Hola soy un children</Box>
       </div>
     )
   }
--- condicional/src/sections/form.js
+++ condicional/src/sections/form.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
 
 export default class Forms extends Component {
-    handleClick(e){
+    constructor() {
+        super()
+        this.state = {
+            inputName: '',
+            inputEdad: 0,
+            inputTerms: true
+        }
+    }
+    handleSubmit = (e) => {
         e.preventDefault()
-        const name = document.getElementById('name').value
+        //const name = document.getElementById('name').value
+        //Manipulando el DOM
+        /*const name = this.inputName.value
         const edad = document.getElementById('edad').value
-        console.log({name, edad})
+        console.log({name, edad})*/
+        console.log(this.state)
+    }
+    handleChange = (e) => {
+        console.log('Change')
+        console.log(e.target.checked)
+        this.setState({ inputTerms: e.target.checked })
     }
     render(){
         return(
             <div>
                 <h4>Formularios</h4>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <p>
-                        <label>Nombre:</label>
-                        <input id="name" name="userName" placeholder="Introduce el nombre"></input>
+                        <label htmlFor="name">Nombre:</label>
+                        <input id="name" name="userName" placeholder="Introduce el nombre"
+                        ref={inputElement => this.inputName = inputElement} value={this.state.inputName} 
+                        onChange ={e => this.setState({ inputName: e.target.value })}
+                        required/>
                     </p>
                     <p>
-                        <label>Edad:</label>
-                        <input id="edad" name="userEdad" placeholder="Introduce la edad"></input>
+                        <label htmlFor="edad">Edad:</label>
+                        <input id="edad" name="userEdad" placeholder="Introduce la edad" value={this.state.inputEdad} 
+                        onChange = {e => this.setState({ inputEdad: e.target.value })}
+                        required></input>
                     </p>
-                    <button onClick={this.handleClick}>Enviar</button>
+                    <p>
+                        <label>
+                            <input onChange={this.handleChange} type="checkbox" checked={this.state.inputTerms}/>
+                            Acepto los términos
+                        </label>
+                    </p>
+                    <button>Enviar</button>
                 </form>
             </div>
         )
