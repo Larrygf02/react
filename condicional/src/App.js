508a395d2d612b37a714473f8caf716359631557
Raul Larriega <raulgf_02@hotmail.com>
2018-10-30 07:54:10 -0500

    ComponentWillRecieve props

--- condicional/src/App.js
+++ condicional/src/App.js
@@ -1,54 +1,14 @@
-import React, { Component } from 'react';
-import ComponentWillMount from './sections/ComponentWillMount';
-class App extends Component {
+import React, {Component} from 'react';
+import ExampleLifeCycle from './sections/LifeCycle.js';
 
-   render() {
-    return(
-      <div className="app">
-        <ComponentWillMount/>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi at facilis eos voluptatem aut alias voluptate molestias saepe repudiandae! Officiis eveniet, cum placeat minus ipsam molestias laboriosam rem enim?</p>
-    <p>Consequuntur minus hic illo optio? Minima sed incidunt esse ipsum molestiae sunt provident nihil cumque et numquam blanditiis harum, vel dolorum aspernatur aliquam repudiandae perferendis, alias nisi voluptatibus eos beatae.</p>
-    <p>Recusandae ex impedit quasi consequatur nisi, voluptate deleniti est dolorem optio temporibus at! Voluptatum neque in quia totam rerum quas numquam magni ex commodi nostrum quidem, eum vero adipisci veritatis.</p>
-    <p>Sapiente, dolor at porro consequatur blanditiis eveniet aliquid accusantium cumque numquam voluptates nisi explicabo, tempore illum. Dolorum laborum voluptate laboriosam sit repellat officia temporibus. Est distinctio nesciunt quos perspiciatis et.</p>
-    <p>Cupiditate, odit id voluptates exercitationem accusantium consequatur maxime tenetur nulla architecto inventore et illo magnam ducimus iste, facere ad. Nam ducimus in reprehenderit voluptatem officiis illo maxime nesciunt totam voluptatum!</p>
-    <p>Quisquam, voluptatem? Itaque consectetur architecto sit ab tempora aliquam quod! Laudantium aliquam impedit porro aut ipsam numquam quis illo minima illum sed suscipit eveniet vero quasi, architecto dolorem id cumque.</p>
-    <p>Suscipit nam omnis aut quo! Minus, asperiores optio deleniti molestiae minima tempore modi, odit, ducimus dolore quasi natus delectus blanditiis alias! Architecto omnis similique autem possimus iusto sunt ut perferendis.</p>
-    <p>Quam, illum. Architecto odit quis blanditiis ex nobis necessitatibus aut saepe. Adipisci inventore libero, eius quibusdam minima provident fugit eveniet suscipit ducimus dicta pariatur dolor atque quia id! Blanditiis, distinctio.</p>
-    <p>Ab possimus error aliquid amet illo assumenda quidem voluptatibus provident asperiores sed qui consequatur repellendus accusantium maxime quaerat quisquam quis, doloribus, adipisci atque nostrum accusamus recusandae corporis magnam dolorem. Eos!</p>
-    <p>Hic temporibus nostrum neque voluptatum, ad aliquam rerum repellendus maiores beatae, voluptate officia voluptates deserunt illo dolore nobis. Vel, optio. Quia, ipsum laudantium mollitia suscipit totam quae quis quisquam debitis?</p>
-    <p>In, recusandae! Reiciendis non magni aperiam dolorum! Nemo voluptatibus, facilis animi aliquid ad accusamus ipsa, rem culpa voluptates quia dolor tempore ullam ut quis dolores cupiditate nulla enim eligendi itaque!</p>
-    <p>Quos temporibus, quae ipsum aliquid doloremque impedit quod sunt voluptatum at neque enim pariatur, aperiam omnis ex adipisci! Cumque eum quo corporis ex velit, inventore laboriosam blanditiis aperiam? Explicabo, quo?</p>
-    <p>Facilis iure, recusandae odit explicabo ullam fugiat non, neque suscipit est quidem corrupti in impedit nisi commodi tempore eveniet ipsum assumenda dolor corporis nemo amet unde, ut accusamus magni. Esse.</p>
-    <p>Eligendi tempora reiciendis ad, perspiciatis voluptatum doloremque nam, inventore explicabo sunt possimus a accusamus distinctio autem doloribus fuga amet. Iste reprehenderit placeat expedita quidem iure adipisci veniam non ad debitis!</p>
-    <p>Porro, dolorem. Minus, dolorem. Iusto at mollitia temporibus architecto tempore saepe sunt tempora alias itaque doloribus? Modi numquam, nostrum assumenda amet quasi dicta tenetur provident corporis. Veritatis magnam voluptate expedita?</p>
-    <p>Quo consectetur, voluptatum laborum molestiae adipisci quaerat inventore velit unde, labore laudantium quasi. Iusto omnis recusandae laborum quo non obcaecati eius, excepturi blanditiis natus veniam pariatur adipisci cum molestias asperiores!</p>
-    <p>Veniam eum cumque tempora consectetur maiores blanditiis ullam repudiandae accusantium voluptate esse quae, similique id hic temporibus tempore quaerat sint, magnam aperiam ut. Ut consectetur vel aut dolorum? Quis, officiis?</p>
-    <p>Quis asperiores atque tempore delectus commodi quibusdam? Nemo corrupti vel rerum suscipit dignissimos, nesciunt magni sed accusantium, minus id commodi earum laudantium facilis non, deleniti repellendus. Consequuntur suscipit itaque neque?</p>
-    <p>Perferendis earum voluptatum modi est blanditiis voluptas, dolore iusto sapiente molestias necessitatibus quod temporibus, culpa, nam perspiciatis. Porro nihil provident quaerat voluptatem, architecto cum, minus repellat assumenda nostrum aspernatur quo!</p>
-    <p>Voluptas voluptate dolores delectus quaerat eos quia, tempore optio eligendi laboriosam architecto dolore voluptates unde quod mollitia nemo asperiores. Voluptatum ipsum autem eligendi fugit corrupti quas blanditiis nobis aperiam debitis.</p>
-    <p>Mollitia iure animi, at veniam laborum, ducimus earum voluptatibus ullam quisquam, quam nesciunt non sit quaerat minus vel! Adipisci sed ut eum qui molestias eaque ea nisi ab eius magni.</p>
-    <p>Omnis vitae tenetur nobis recusandae ut temporibus ducimus veniam velit iusto dignissimos. Alias nostrum id consequuntur qui odio! Nihil, vel! Dolor pariatur veniam earum et quas. Illo, laborum quis? Modi?</p>
-    <p>Voluptatibus ipsa nostrum praesentium iste aliquam. Nulla dolore dolorem, aliquid ratione ducimus minus quasi velit nobis repellat porro, eaque fugiat ullam neque similique voluptatum. Ad optio sunt commodi dolor sint.</p>
-    <p>Ut, assumenda quas dignissimos dolor nam laboriosam, velit rerum dolorem sint quaerat possimus pariatur praesentium molestias! Odio cumque repellendus fuga commodi alias earum quod debitis quibusdam accusamus, exercitationem maiores aliquam?</p>
-    <p>Aut, ea blanditiis! Libero maiores quis blanditiis porro sed recusandae minima, eum quos voluptas illum illo, unde nesciunt enim hic consectetur necessitatibus reprehenderit ad. Expedita maxime explicabo omnis nostrum dicta!</p>
-    <p>Temporibus ratione enim voluptates, mollitia error at corporis eligendi itaque soluta velit similique nostrum esse reiciendis vitae animi suscipit quasi rerum odio maiores quidem saepe tempora. Quo nobis non consequatur.</p>
-    <p>Cumque id qui ipsa eaque magnam provident dicta assumenda dolores temporibus dolorum, voluptas asperiores eum quaerat facere nisi illum! Alias facilis aut quam unde at ipsum odit maxime molestiae iusto?</p>
-    <p>Nobis officia impedit alias facere beatae delectus numquam temporibus sapiente. Repellat beatae quod quo nostrum nesciunt eius quia sit molestias sint enim in quas, quibusdam, placeat sequi! Praesentium, possimus ratione.</p>
-    <p>Id asperiores animi eius facere, quo nobis ullam rem veritatis, autem at sequi vitae repellendus velit neque sit eveniet aspernatur totam quos vero, sint mollitia nemo. Perferendis omnis quasi quo!</p>
-    <p>Inventore voluptate quos distinctio dolorem adipisci incidunt ab maxime nesciunt aspernatur fuga molestias voluptatem sequi blanditiis, impedit, ea fugiat? Consequatur exercitationem autem odio delectus itaque, ducimus accusantium alias explicabo natus!</p>
-    <p>Sint praesentium natus explicabo tempora reprehenderit optio quae hic saepe consequatur animi! Rerum quia similique omnis expedita laborum corporis vero harum, mollitia dicta? Voluptatum suscipit mollitia ducimus veniam quod eaque!</p>
-    <p>Cum eaque culpa optio doloribus nobis architecto, voluptates sed possimus illum placeat. Cumque nam commodi minima! Ipsum saepe ratione odit, illo, laudantium vitae eum aliquid culpa in, sed repellat quis?</p>
-    <p>Commodi voluptas vel dolores ea. Animi in, quod blanditiis autem sapiente corporis voluptatibus dolores reiciendis sit officia ducimus tempora! Odio aliquid accusantium quibusdam facilis enim labore fugiat, autem quia sunt?</p>
-    <p>Corporis amet eius a sequi cum obcaecati odit! Eos quia nobis ullam, repellat asperiores nihil voluptas dignissimos vero corporis dolore unde molestiae veniam, ratione beatae eligendi officiis, fugit praesentium delectus?</p>
-    <p>Deserunt officiis minima consequuntur perferendis modi voluptatem? Illum ut mollitia fugit porro placeat! Eligendi est totam repellat necessitatibus soluta minus veniam dignissimos ratione quasi veritatis. Explicabo tenetur tempora quia fugit!</p>
-    <p>Consectetur porro voluptates aspernatur, tenetur, pariatur repudiandae adipisci enim sunt veritatis, ut expedita velit ipsam nesciunt quas cupiditate. Fugiat minus eos, repellat fugit earum nostrum voluptas alias velit voluptatem nam!</p>
-    <p>Accusantium amet recusandae excepturi eos. Nesciunt nulla magnam inventore atque saepe repellat doloremque officiis sunt, excepturi, voluptas architecto aperiam illo? Ad perspiciatis recusandae quos alias sequi molestiae dicta sed fugiat.</p>
-    <p>Praesentium sapiente reiciendis, hic aliquam esse dolores et perspiciatis ipsum optio vel beatae. Hic, tempora. Dolorem ab, eligendi ipsum, excepturi et dignissimos distinctio rerum nulla quibusdam accusamus eaque, facilis non.</p>
-    <p>At rem a ipsum laboriosam non, minima, unde qui nulla magni saepe rerum quis autem laborum vero nihil quam quas? Non autem optio nihil quam officiis officia aliquid magni necessitatibus!</p>
-    <p>Laudantium dolorem dolore amet exercitationem, velit molestiae libero nisi error voluptatum maiores, natus officia excepturi. Ipsam numquam odit placeat! Modi, quae maiores. Expedita, voluptates adipisci. In, amet odio? Assumenda, dignissimos.</p>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ExampleLifeCycle></ExampleLifeCycle>
       </div>
     )
-  } 
+  }
 }
 
 export default App;
