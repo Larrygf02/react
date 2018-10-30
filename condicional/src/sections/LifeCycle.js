95a2281e186b5031e952a86df367c460e82a641f
Larrygf02 <raulgf_02@hotmail.com>
2018-10-30 10:18:21 -0500

    Refactor willRecieveProps

--- condicional/src/sections/LifeCycle.js
+++ condicional/src/sections/LifeCycle.js
@@ -6,14 +6,12 @@ const ANIMAL_IMAGES = {
   dolphin: 'https://goo.gl/BbiKCd',
   panda: 'https://goo.gl/oNbtoq',
 }
+const ANIMALS = Object.keys(ANIMAL_IMAGES)
 
 class AnimalImage extends Component {
     state = { src: ANIMAL_IMAGES[this.props.animal]}
     componentWillReceiveProps (nextProps) {
-        if (this.props.animal !== nextProps.animal){
-            console.log('setState')
-            this.setState({ src: ANIMAL_IMAGES[nextProps.animal]})
-        }
+        this.setState({ src: ANIMAL_IMAGES[nextProps.animal]})
     }
     render(){
         console.log('-> render')
@@ -27,20 +25,34 @@ class AnimalImage extends Component {
 }
 
 AnimalImage.propTypes = {
-    animal: PropTypes.oneOf(['cat','dolphin','panda'])
+    //animal: PropTypes.oneOf(['cat','dolphin','panda'])
+    animal: PropTypes.oneOf(ANIMALS)
 }
 AnimalImage.defaultProps = {
     animal: 'panda'
 }
 class ExampleLifeCycle extends Component {
     state = { animal: 'panda'}
+  _renderAnimalButton = (animal) => {
+      return(
+          <button 
+          disabled={animal === this.state.animal}
+          key={animal} 
+          onClick={() => this.setState({ animal })}
+          >{ animal }</button>
+      )
+  }
   render() {
     return (
       <div>
         <h4>Ciclo de actualizacion: Ejemplo ComponentWillRecieveProps</h4>
-        <button onClick={() => this.setState({animal: 'panda' })}> Panda</button>
+        {ANIMALS.map(this._renderAnimalButton)}
+        {/*this._renderAnimalButton('panda')*/}
+        {/*this._renderAnimalButton('cat')*/}
+        {/*this._renderAnimalButton('dolphin')*/}
+        {/* <button onClick={() => this.setState({animal: 'panda' })}> Panda</button>
         <button onClick={() => this.setState({animal: 'cat' })}> Cat</button>
-        <button onClick={() => this.setState({animal: 'dolphin' })}> Dolphin</button>
+        <button onClick={() => this.setState({animal: 'dolphin' })}> Dolphin</button> */}
         <AnimalImage animal={this.state.animal }/>
       </div>
     )
